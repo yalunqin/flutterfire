ff0ca68d7f94eface51188d8f4263dfd73e95568
Arthur Thompson <arthurthompson@google.com>
2021-06-10 15:55:31 -0700

    refactor: remove unnecessary methods and properties from performance platform interface

--- packages/firebase_performance/firebase_performance_platform_interface/lib/src/platform_interface/platform_interface_firebase_performance.dart
+++ packages/firebase_performance/firebase_performance_platform_interface/lib/src/platform_interface/platform_interface_firebase_performance.dart
@@ -6,8 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 import '../method_channel/method_channel_firebase_performance.dart';
-import 'platform_interface_trace.dart';
 import 'platform_interface_http_metric.dart';
+import 'platform_interface_trace.dart';
 
 enum HttpMethod { Connect, Delete, Get, Head, Options, Patch, Post, Put, Trace }
 
@@ -22,21 +22,14 @@ enum HttpMethod { Connect, Delete, Get, Head, Options, Patch, Post, Put, Trace }
 /// [FirebasePerformancePlatform] methods.
 abstract class FirebasePerformancePlatform extends PlatformInterface {
   /// Create an instance using [app].
-  FirebasePerformancePlatform({this.appInstance}) : super(token: _token);
+  FirebasePerformancePlatform({this.appInstance}) : super(token: Object());
 
   /// Create instance using [app] using the existing implementation.
   factory FirebasePerformancePlatform.instanceFor({
     required FirebaseApp app,
-    Map<dynamic, dynamic>? pluginConstants,
   }) {
-    return FirebasePerformancePlatform.instance
-        .delegateFor(app: app)
-        .setInitialValues(
-          performanceValues: pluginConstants ?? <dynamic, dynamic>{},
-        );
+    return FirebasePerformancePlatform.instance.delegateFor(app: app);
   }
-
-  static final Object _token = Object();
 
   static FirebasePerformancePlatform? _instance;
 
@@ -46,12 +39,6 @@ abstract class FirebasePerformancePlatform extends PlatformInterface {
   /// if no other implementation was provided.
   static FirebasePerformancePlatform get instance {
     return _instance ??= MethodChannelFirebasePerformance.instance;
-  }
-
-  /// Sets the [FirebasePerformancePlatform] instance.
-  static set instance(FirebasePerformancePlatform instance) {
-    PlatformInterface.verifyToken(instance, _token);
-    _instance = instance;
   }
 
   /// The [FirebaseApp] this instance was initialized with.
@@ -66,18 +53,6 @@ abstract class FirebasePerformancePlatform extends PlatformInterface {
   @protected
   FirebasePerformancePlatform delegateFor({required FirebaseApp app}) {
     throw UnimplementedError('delegateFor() is not implemented');
-  }
-
-  /// Sets any initial values on the instance.
-  ///
-  /// Platforms with Method Channels can provide constant values to be
-  /// available before the instance has initialized to prevent unnecessary
-  /// async calls.
-  @protected
-  FirebasePerformancePlatform setInitialValues({
-    required Map<dynamic, dynamic> performanceValues,
-  }) {
-    throw UnimplementedError('setInitialValues() is not implemented');
   }
 
   /// Only works for native apps. Always returns true for web apps.
